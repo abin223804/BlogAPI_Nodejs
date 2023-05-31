d94a361334f7c06a838e629e1f189509e5a143aa
Abin s chandran <abinschandran@gmail.com>
2023-05-31 11:43:53 +0530

    change viewengine

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const path = require("path");
-const ejs = require("ejs");
+const hbs = require("express-handlebars");
+const Handlebars = require("handlebars");
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
 
 
 
-app.set('view',path.join(__dirname,'views/user'));
-app.set('view engine',"ejs");
 
-
+const session = require("express-session");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,7 +21,18 @@ app.use(express.urlencoded({ extended: true }));
 
 const Route = require("./route/route")
 
-
+Route.set("views", path.join(__dirname, "view/user"));
+Route.set("view engine", "hbs");
+Route.engine(
+  "hbs",
+  hbs.engine({
+    extname: "hbs",
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
+    defaultLayout: "Layout",
+    layoutsDir: __dirname + "/view/Layout",
+    partialsDir: __dirname + "/view/partials",
+  })
+);
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -45,8 +58,8 @@ const db = require('./models');
 (async () => {
   try {
     await db.sequelize.sync();
-    app.listen(4000, () => {
-      console.log('Server listening on port 4000');
+    app.listen(5000, () => {
+      console.log('Server listening on port 5000');
     });
   } catch (error) {
     console.error('Error occurred while synchronizing the database:', error);
